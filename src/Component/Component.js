dab2a1f966158927ec5c00a593486d44bf1bc6ba
Bhargav Pulipati <pulipati.bhargav@gmail.com>
2021-09-25 21:29:44 +0530

    updated

--- src/Component/Component.js
+++ src/Component/Component.js
@@ -2,7 +2,7 @@
 
 function clickme(){
     return <div>
-        <h1> 🤭   Hello, Harsha Vardhan! I am Bhargav's bot. 🤖  </h1>
+        <h1> 🤭   Hello, Sai Gopal! I am Bhargav's bot. 🤖  </h1>
         <h2>  👦 Please kick start his bike 🏍 every Sunday until my OWNER comes back. 😍</h2>
         
     </div>
